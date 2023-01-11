dfa9cf44ce3c1e7ed111f6220794cd1040b913d3
Ann S <gb9363@mail.ru>
2023-01-12 00:47:47 +0300

    CL1_001 с проверкой ввода

---
+++ CL1_001sproverkoi/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+
+Например,
+4 -> 16
+-3 -> 9
+-7 -> 49
+*/
+
+int GetNumber (string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            return result;
+        }
+        else
+            Console.WriteLine("Ввели не число");
+    }
+    return result;
+}
+
+int number = GetNumber("Введите число: ");
+int result = number * number;
+
+Console.WriteLine($"Квадрат числа {number} равен {result}");
