44af687067ab6f748250ce68524875ad54db3001
Ann S <gb9363@mail.ru>
2023-01-12 00:34:33 +0300

    CL1_001 new

--- CL1_001/Program.cs
+++ CL1_001/Program.cs
@@ -9,3 +9,8 @@
 
 Console.WriteLine("Введите число: ");
 string numberStr = Console.ReadLine() ?? "";
+int number = Convert.ToInt32(numberStr);
+
+int result = number * number;
+
+Console.WriteLine($"Квадрат числа {number} равен {result}");
