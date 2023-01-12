d89e0991e05fee61aaf4fdb2e0632783a8725f0a
Ann S <gb9363@mail.ru>
2023-01-12 06:21:55 +0300

    CL1 Home Задача 2

---
+++ CL1_Home1_2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.WriteLine("Введите первое число: ");
+string num1 = Console.ReadLine() ?? "";
+int first = Convert.ToInt32(num1);
+
+Console.WriteLine("Введите второе число: ");
+string num2 = Console.ReadLine() ?? "";
+int second = Convert.ToInt32(num2);
+
+if (first > second)
+{
+    Console.WriteLine($"Первое число {first} больше чем второе число {second}");
+} 
+else
+Console.WriteLine($"Первое число {first} меньше чем второе число {second}");
